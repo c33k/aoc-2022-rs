57fb0724882c6f884003d0edb4e5c63c712f61f9
Rafael L. Toscano <rltoscano@gmail.com>
2023-09-24 22:45:43 +0200

    DAY 3 - part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn get_value(c: &char) -> u32 {
     // a-z - 1 to 26
@@ -22,8 +23,6 @@ pub fn part1(path: &str) -> Result<u32, std::io::Error> {
         // 1 - go through first half of the string, adding each char to Set
         // 2 - go through second half and find repeated character
         // 3 - get priority value of character and add to sum
-        println!(">>>>>>>>> STARTING {line}");
-
         letters_set.clear();
         
         let half = (line.len() as i32) / 2; 
@@ -31,15 +30,54 @@ pub fn part1(path: &str) -> Result<u32, std::io::Error> {
         for (i, c) in line.chars().enumerate() {
             if (i as i32) < half {
                 letters_set.insert(c);
-                println!("Adding {} to SET", c);
             } else {
                 if letters_set.contains(&c) {
-                    println!("FOUND on second half: value of  {} is {}", c, get_value(&c));
                     sum += get_value(&c);
                     break;
                 }
             }
         } 
+    }
+
+    Ok(sum)
+}
+
+pub fn part2(path: &str) -> Result<u32, std::io::Error> {
+    let rucksacks = std::fs::read_to_string(path)?;
+    
+    let mut letters: HashMap<char, u32> = HashMap::new();
+    let mut sum = 0;
+
+    for (i, line) in rucksacks.lines().enumerate() {
+        // Each group has 3 lines. So we use MOD to keep track of line in group
+        let cur_line_in_group = (i % 3) as u32;
+
+        if cur_line_in_group == 0 { 
+            letters.clear();
+        }
+    
+        for c in line.chars() {
+            if letters.contains_key(&c) {
+                // if we are on last line of the group and the letter appeared
+                // ... on the previous two groups, se add it's value to sum.
+                if cur_line_in_group == 2 && *letters.get(&c).unwrap() == 1 {
+                    sum += get_value(&c);
+                    break;
+                } else {
+                    // already found this in the past, so increment the group
+                    if let Some(x) = letters.get_mut(&c) {
+                        *x = cur_line_in_group;
+                    }
+                }
+            } else {
+                // if it is not inserted and it is in the first group, we insert
+                // ... it. But if it is not in the first group, it means it 
+                // ... didn't appear on previous groups. So no need to count it.
+                if cur_line_in_group == 0 {
+                    letters.insert(c, cur_line_in_group);
+                }
+            }
+        }
     }
 
     Ok(sum)
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main() {
     score = day2::part1("./day2/strategy.txt")
         .expect("ERROR TRYING TO RUN DAY2 PART 1 CHALLENGE");
     println!("RESULT DAY 2 - part 1: {score}");
-
     score = day2::part2("./day2/strategy.txt")
         .expect("ERROR TRYING TO RUN DAY2 PART 2 CHALLENGE");
     println!("RESULT DAY 2 - part 2: {score}");
@@ -18,5 +17,9 @@ fn main() {
     score = day3::part1("./day3/rucksack_reorganization.txt")
         .expect("ERROR TRYING TO RUN DAY3 PART 1");
     println!("RESULT DAY 3 - part 1: {score}");
+    score = day3::part2("./day3/rucksack_reorganization.txt")
+        .expect("ERROR TRYING TO RUN DAY3 PART 2");
+    println!("RESULT DAY 3 - part 2: {score}");
+
 }
 
